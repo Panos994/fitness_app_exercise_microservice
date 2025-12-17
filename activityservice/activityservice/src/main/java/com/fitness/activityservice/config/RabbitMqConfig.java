36a28b6a6b89eadfbd3fb8a2e73cb1477e45a911
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2025-12-17 13:31:05 +0200

    API_GATEWAY

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -1,18 +1,16 @@
 package com.fitness.activityservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class RabbitMqConfig {
+
     @Value("${spring.rabbitmq.queue.name}")
     private String queue;
 
@@ -21,29 +19,24 @@ public class RabbitMqConfig {
 
     @Value("${spring.rabbitmq.routing.key}")
     private String routingKey;
-    @Bean
-    public Queue activityQueue(){
-        return new Queue("activity.queue",true);
-    }
-    // 2. Exchange (Προσθήκη)
-    @Bean
-    public TopicExchange activityExchange() {
-        // Χρησιμοποιούμε TopicExchange, καθώς το routing key υποδηλώνει αυτό το pattern.
-        return new TopicExchange("fitness.exchange");
-    }
 
-    // 3. Binding (Σύνδεση Exchange με Queue μέσω Routing Key)
     @Bean
-    public Binding binding(Queue activityQueue, TopicExchange activityExchange) {
-        // Χρησιμοποιούμε το routing key από το application.yml
-        return BindingBuilder
-                .bind(activityQueue)
-                .to(activityExchange)
-                .with("activity.tracking");
+    public Queue activityQueue() {
+        return new Queue(queue, true);
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter(){
+    public DirectExchange activityExchange() {
+        return new DirectExchange(exchange);
+    }
+
+    @Bean
+    public Binding activityBinding(Queue activityQueue, DirectExchange activityExchange) {
+        return BindingBuilder.bind(activityQueue).to(activityExchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 }
