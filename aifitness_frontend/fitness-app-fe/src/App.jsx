dc99440b7e856474aad01c5fb53454d5167a0918
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2026-01-08 13:04:30 +0200

    login-fe

--- aifitness_frontend/fitness-app-fe/src/App.jsx
+++ aifitness_frontend/fitness-app-fe/src/App.jsx
@@ -1,10 +1,38 @@
 import { Button } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
+import { AuthContext } from "react-oauth2-code-pkce"
+import { useDispatch } from "react-redux";
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from "react-router";
+import { setCredentials } from "./store/authSlice";
+
 
 function App() {
+  const {token,tokenData, logIn,logOut, isAuthenticated} = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+  useEffect(() => {
+    if (token){
+      dispatch(setCredentials({token, user: tokenData}));
+      setAuthReady(true);
+    }
+  }, [token,tokenData,dispatch]);
+
   return (
-    <div style={{ padding: 16 }}>
-      <Button variant="contained" color="primary">LOGIN</Button>
-    </div>
+    <Router>
+      {!token ? (
+      <Button variant="contained" color="#dc004e"
+            onClick={()=>{
+              logIn();  // call login() function.
+            }}>LOGIN</Button>
+          ) : (
+            <div><pre>
+                  {JSON.stringify(tokenData, null, 2)}
+                   {JSON.stringify(token, null, 2)}    
+              </pre></div>
+          )}
+    </Router> 
+      
+    
   )
 }
 
