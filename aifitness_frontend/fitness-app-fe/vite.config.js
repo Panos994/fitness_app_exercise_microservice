5609ac191b4987a2ec785f5bf788c2e4bfd4bd95
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2026-01-11 22:32:37 +0200

    fe updates

--- aifitness_frontend/fitness-app-fe/vite.config.js
+++ aifitness_frontend/fitness-app-fe/vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      // Επιτρέπει στο Vite να σερβίρει αρχεία έξω από το root αν χρειαστεί
-      allow: ['..'] 
-    }
-  },
-  // Αναγκάζουμε το Vite να χρησιμοποιεί σχετικές διαδρομές
-  base: './'
+  resolve: {
+    // Αυτό εμποδίζει το Vite από το να "μεταφράζει" το fitness-link 
+    // στην πραγματική διαδρομή με τα κενά και το "&"
+    preserveSymlinks: true
+  }
 })
