f261eaf480acc99f7f6043b45057494d9738724b
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2026-01-15 11:26:53 +0200

    fe axios api

---
+++ aifitness_frontend/fitness-app-fe/src/services/api.js
@@ -0,0 +1,13 @@
+const API_URL = 'http://localhost:9192/api/'
+
+const api = axios.create({
+  baseURL: API_URL
+})
+
+api.interceptors.request.use((config) => {
+
+});
+
+export const getActivities = () => api.get('/activities'); 
+export const addActivity = () => api.post('/activity', activity); 
+export const getActivityDetail = () => api.get('/recommendations/activity/${id}', activity);
