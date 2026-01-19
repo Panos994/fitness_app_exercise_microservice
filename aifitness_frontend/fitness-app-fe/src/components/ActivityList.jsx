9797ba81212cc6f3ca1f79bef64ae5847d93b5ce
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2026-01-19 11:38:16 +0200

    minor

--- aifitness_frontend/fitness-app-fe/src/components/ActivityList.jsx
+++ aifitness_frontend/fitness-app-fe/src/components/ActivityList.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import {Grid2} from '@mui/material';
 const ActivityList = () => {
     return (
-        <div>Activity List</div>
+        <Grid2 container spacing={2}>
+  
+        </Grid2>
     )
 }
 
