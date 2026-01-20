0bafefaf80ed3b2241cf4ede215fb0ec083d9c64
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2026-01-20 10:24:24 +0200

    fe minor update

--- aifitness_frontend/fitness-app-fe/src/components/ActivityList.jsx
+++ aifitness_frontend/fitness-app-fe/src/components/ActivityList.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import {Grid2} from '@mui/material';
+import {Grid2, Typography} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 const ActivityList = () => {
+    const [activities, setActivities] = userState([]);
+    const navigate = useNavigate();
+
+    const fetchActivities = async () => {
+        try {
+            const response = await getActivities();
+            setActivities(response.data);
+        }catch(error){
+            console.error(error);
+        }
+    }
     return (
         <Grid2 container spacing={2}>
-  
-        </Grid2>
-    )
-}
+            {activities.map((activity) =>(
+                <Grid2 container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        <Card sx={{cursos: 'pointer'}} onClick {() => navigate(`/activities/${activity.id}`)}>
+                    <CardContent>
+                        <Typography variant='h6'>{activity.type}</Typography>
+                        <Typography>Duration: {activity.duration}</Typography>
+                        <Typography>Calories: {activity.caloriesBurned}</Typography>
+                    </CardContent>
 
-export default ActivityList
+                        </Card>
+                </Grid2>
+            ))}
+        </Grid2>
+    );
+};
+
+export default ActivityList;
