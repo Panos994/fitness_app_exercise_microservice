ea56f8eb6cd86e55118713290d14bd6a0a603bf1
Panos Foteinopoulos <118283261+Panos994@users.noreply.github.com>
2026-01-21 11:11:13 +0200

    fe update v3

--- aifitness_frontend/fitness-app-fe/src/components/ActivityDetail.jsx
+++ aifitness_frontend/fitness-app-fe/src/components/ActivityDetail.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import { Typography } from '@mui/material';
+import React, { act, use, useEffect, useState } from 'react'
 
 const ActivityDetail = () => {
+    const {id} = useParams();
+    const [activity, setActivity] = useState(null);
+    const [recommendation, setRecommendation] = useState(null);
+
+    useEffect(() => {
+        const fetchUserActivityDetail = async () => {
+            try {
+                const response = await getActivityDetail(id);
+                setActivity(response.data);
+                setRecommendation(response.data.recommendation);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchUserActivityDetail();
+    }, [id]);
+    if(!activity){
+    return (
+        <Typography>Loading...</Typography>
+    )}
     return (
         <div>ActivityDetail</div>
     )
